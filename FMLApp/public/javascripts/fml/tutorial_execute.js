7add7397d3647f0f9922bc4d889cc4d7f66eed47
Geoffrey Alexandre <geoffrey.alexandre@inria.fr>
2014-06-26 10:28:05 +0200

    I added some comments

--- FMLApp/public/javascripts/fml/tutorial_execute.js
+++ FMLApp/public/javascripts/fml/tutorial_execute.js
@@ -1,17 +1,37 @@
+/**
+ *Function which send the code to the compiler
+ *and displays the result.
+ */
 function FMLEditorCtrl($scope, $rootScope) {
-	
+	/*
+	 *when we click on the button execute
+	 */
 	$scope.cmd = function () {
+		/*
+		* We get the code from the editor
+		*/
 	    var idToGet = editor.getSession().getValue();
 	    jsRoutes.controllers.WebFMLInterpreter.interpret(idToGet).ajax({
+			/*
+			 *if the code is correctly compile
+			 */
 			success : function(data) {
+				/*display the result*/
 				$rootScope.$broadcast('variables', data)
-				//$('#msgid').html('<div class="alert alert-success">' + data.lastVar + '</div>') ; 
+				/*display nothing in this div*/
+				$('#msgid').html('') ; 
 			},
-		        error : function(data) {  
+			/*
+			*if error
+			*/
+		        error : function(data) {
+				/*display nothing in this div*/
+				$rootScope.$broadcast('variables','')
+				/*display the result*/
 				$('#msgid').html('Error...<div class="alert alert-danger">' + data.lastVar + '</div>') ; 
 			},
 		        beforeSend : function(event, jqxhr, settings) {
-			        $('#msgid').html('<img src="assets/images/ajax-loader.gif" />') ; 
+			        $('#wait').html('<img src="assets/images/ajax-loader.gif" />') ; 
 			},
 		       complete : function(jqxhr, textstatus) {
 			    $('#wait').html('') ;		   
